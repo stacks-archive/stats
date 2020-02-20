c83787d71d0a89cae369b882c40d3203c55f7604
Hank Stoever <hstove@gmail.com>
2020-02-20 12:08:10 -0800

    feat: adds test app

---
+++ packages/test-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Flex } from '@blockstack/ui';
+import { event } from '@blockstack/analytics';
+
+const App: React.FC = () => {
+  return (
+    <Flex align="center" justify="center" width="100vw" minHeight="100vh" className="App" direction="column">
+      <Button onClick={() => event({ name: 'testing' }) }>Hello</Button>
+    </Flex>
+  );
+};
+
+export default App;
